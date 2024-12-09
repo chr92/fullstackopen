a1ff27c5f2282e8bd77ea5d1e3bcb4b4b76ded0c
Christiaan Hendriksen <christiaan.hendriksen@gmail.com>
2024-12-09 17:26:41 +0000

    up to 1.8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,35 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const Title = () => <h1>give feedback</h1>;
+
+const UpdateFeedback = (typeOfFeedback, setFeedback) => {
+  return () => {
+    setFeedback((prev) => {
+      const newFeedback = {
+        ...prev,
+        [typeOfFeedback]: prev[typeOfFeedback] + 1,
+      };
+      const total = newFeedback.good + newFeedback.neutral + newFeedback.bad;
+      newFeedback.average = total
+        ? (newFeedback.good - newFeedback.bad) / total
+        : 0;
+      newFeedback.positive = total ? (newFeedback.good / total) * 100 : 0;
+      return newFeedback;
+    });
+  };
+};
+
+const Buttons = ({ setFeedback }) => {
+  return (
+    <div>
+      <button onClick={UpdateFeedback("good", setFeedback)}>good</button>
+      <button onClick={UpdateFeedback("neutral", setFeedback)}>neutral</button>
+      <button onClick={UpdateFeedback("bad", setFeedback)}>bad</button>
+    </div>
+  );
+};
+
+const Statistics = ({ feedback }) => {
+  return (
+    <div>
+      <h1>statistics</h1>
+      <p>good {feedback.good}</p>
+      <p>neutral {feedback.neutral}</p>
+      <p>bad {feedback.bad}</p>
+      <Feedback {feedback} />
+    </div>
+  );
+};
+
+const App = () => {
+  const [feedback, setFeedback] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+    average: 0,
+    positive: 0,
+  });
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <Title />
+      <Buttons setFeedback={setFeedback} />
+      <Statistics feedback={feedback} />
+    </div>
+  );
+};
 
-export default App
+export default App;
